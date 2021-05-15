4e3a674a03187c58b0955a838eb612391e632292
Ivan <ivan.kashytski.mkx@gmail.com>
2021-05-15 20:44:29 +0300

    Add Ads

---
+++ Assets/Scripts/Ads_Script.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
+
+public class Ads_Script : MonoBehaviour
+{
+    [SerializeField] Text winText;
+
+    void Start()
+    {
+        if (Advertisement.isSupported)
+            Advertisement.Initialize("4130595",false);
+    }
+    void Update()
+    {
+        if (winText.text != "")
+            if (Advertisement.IsReady())
+                Advertisement.Show();
+    }
+}
